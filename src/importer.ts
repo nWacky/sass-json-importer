4a87241230be3de5f66ecfcb076718887f8dbc9e
nWacky <gosha.evtushenko@gmail.com>
2025-06-10 17:44:48 +0300

    fix white color

--- src/importer.ts
+++ src/importer.ts
@@ -91,7 +91,8 @@ export default class JsonImporter implements Importer {
 	}
 
 	// isJsonFile = (url: string): boolean => /\.js(on5?)?$/.test(url);
-	isJsonFile = (url: string): boolean => url.endsWith('.json') || url.endsWith('.jsonc');
+	isJsonFile = (url: string): boolean =>
+		url.endsWith('.json') || url.endsWith('.jsonc');
 
 	protected ensureObject(
 		jsonContent: JsonObject,
@@ -170,7 +171,7 @@ export default class JsonImporter implements Importer {
 	}
 
 	protected parseMap(jsonContent: JsonObject) {
-		return `(${this.processKeys(jsonContent, (key, value) => `${key}: ${this.parseValue(value)}`).join(',')})`;
+		return `(${this.processKeys(jsonContent, (key, value) => `"${key}": ${this.parseValue(value)}`).join(',')})`;
 	}
 
 	protected parseList(list: JsonValue[]) {
